523ccf9cd1fd6a6fdf3d732350d7ff1fcb50ba2b
Daniel Moreno <danielvdmlfiis@gmail.com>
2018-03-09 22:17:38 -0500

    Creación de Componente Employee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
-import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent,
-    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule
