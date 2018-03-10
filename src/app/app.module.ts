93e7958854d779f942a4ccaaacfb74f3c6fe7c69
Daniel Moreno <danielvdmlfiis@gmail.com>
2018-03-09 22:16:58 -0500

    Creación de Componente Employee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+   selector:"employee",
+   template:"<span>nombre</span>"
+})
+export class EmployeeComponent{
+    public name="Victor Daniel"
+}
