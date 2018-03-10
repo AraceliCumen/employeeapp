67ff315dcc781c2f9caa8da19bf48e3809a9331a
Daniel Moreno <danielvdmlfiis@gmail.com>
2018-03-10 00:05:00 -0500

    Directiva [ngSwitch]

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -12,7 +12,7 @@ export class EmployeeComponent{
     constructor(){
         this.employees=[
             new Employee(1,"Julio Cesar","juliocesar@gmail.com","11/07/1990","Lima","999999999",5000,"Soluciones Tecnológicas","","M"),
-            new Employee(2,"Cinthya Alejandra","cinthya@gmail.com","20/08/1990","Lima","999999999",3000,"Soluciones Tecnológicas","","F"),
+            new Employee(2,"Cinthya Alejandra","cinthya@gmail.com","20/08/1990","Lima","999999999",3000,"Soluciones Tecnológicas","",""),
             new Employee(3,"Flavia Ramos","flavia@gmail.com","4/07/1992","Lima","999999999",2000,"Soluciones Tecnológicas","","F"),
             new Employee(4,"Juan Ramirez","juanramirez@gmail.com","9/07/1995","Lima","999999999",15000,"Soluciones Tecnológicas","","M"),
             new Employee(5,"Simón Bolivar","simonvolivar@gmail.com","1/07/1999","Lima","999999999",2100,"Soluciones Tecnológicas","","M")
