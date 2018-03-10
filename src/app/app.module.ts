a93d41d26228fa9af43fe74d63af5b259c1aeafa
Daniel Moreno <danielvdmlfiis@gmail.com>
2018-03-10 02:09:04 -0500

    Creación de component employee-detail con scaffold: 'ng generate component employee-detail'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { routing,appRoutingProviders } from "./app.routing";
 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent,
+    EmployeeDetailComponent,
   ],
   imports: [
     BrowserModule,
