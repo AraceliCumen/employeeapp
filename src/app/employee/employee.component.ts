5cce80b03ffd6bab33ad600fe85e8e096d425a15
Daniel Moreno <danielvdmlfiis@gmail.com>
2018-03-09 23:07:01 -0500

    Usando Array de Empleados, y directiva *ngFor

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -7,8 +7,15 @@ import {Employee} from "./employee";
 })
 export class EmployeeComponent{
     public employee:Employee
+    public employees:Array<Employee>
 
     constructor(){
-        this.employee=new Employee("Julio Cesar","juliocesar@gmail.com","11/07/1990","Lima","999999999",5000,"Soluciones Tecnológicas");
+        this.employees=[
+            new Employee(1,"Julio Cesar","juliocesar@gmail.com","11/07/1990","Lima","999999999",5000,"Soluciones Tecnológicas",""),
+            new Employee(2,"Cinthya Alejandra","cinthya@gmail.com","20/08/1990","Lima","999999999",5000,"Soluciones Tecnológicas",""),
+            new Employee(3,"Flavia Ramos","flavia@gmail.com","4/07/1992","Lima","999999999",5000,"Soluciones Tecnológicas",""),
+            new Employee(4,"Juan Ramirez","juanramirez@gmail.com","9/07/1995","Lima","999999999",5000,"Soluciones Tecnológicas",""),
+            new Employee(5,"Simón Bolivar","simonvolivar@gmail.com","1/07/1999","Lima","999999999",5000,"Soluciones Tecnológicas","")
+        ]
     }
 }
--- src/app/employee/employee.ts
+++ src/app/employee/employee.ts
@@ -1,11 +1,13 @@
 export class Employee{
     constructor(
+        public id:number,
         public name:string,
         public email:string,
         public birthday:string,
         public city:string,
         public phone:string,
         public salary:number,
-        public company:string
+        public company:string,
+        public photoURL:string
     ){}
 }
